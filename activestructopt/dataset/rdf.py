df1ef56b2539f7c11d78c4423dc79ec627ba3aa7
itsgt <islagle3@gatech.edu>
2023-05-29 18:47:15 -0400

    Add normalization

--- activestructopt/dataset/rdf.py
+++ activestructopt/dataset/rdf.py
@@ -5,9 +5,10 @@ def get_dist(a, b):
   return np.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2)
 
 def get_rdf(structure, rs, σ):
+  p = len(target_structure) / target_structure.volume
   rmax = np.max(rs) + 3 * σ
   dists = []
   for i in range(len(structure)):
     neighbors = structure.get_sites_in_sphere(structure.sites[i].coords, rmax)
     dists.extend(list(filter(lambda x: x > 0, map(lambda n: get_dist(structure.sites[i].coords, n.coords), neighbors))))
-  return sum(map(lambda d: norm.pdf(rs, d, σ), dists))
+  return sum(map(lambda d: norm.pdf(rs, d, σ), dists)) / (p * (4/3) * np.pi * rs ** 3)
