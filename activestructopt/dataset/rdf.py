ada9c2e77f1013ef4922aa28e01eddaefe151dca
itsgt <islagle3@gatech.edu>
2023-06-19 15:30:28 -0400

    Trying a faster rdf function

--- activestructopt/dataset/rdf.py
+++ activestructopt/dataset/rdf.py
@@ -4,11 +4,17 @@ import numpy as np
 def get_dist(a, b):
   return np.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2)
 
-def get_rdf(structure, rs = np.arange(0.1, 10.0, 0.001), σ = 0.05):
+def get_rdf(structure, σ = 0.05):
+  rs = np.arange(0.0, 12.0, 0.001)
+  full_rs = np.arange(-14.0, 14.0, 0.001)
+  norm_pdf = norm.pdf(full_rs, 0.0, σ)
   p = len(structure) / structure.volume
   rmax = np.max(rs) + 3 * σ
   dists = []
   for i in range(len(structure)):
     neighbors = structure.get_sites_in_sphere(structure.sites[i].coords, rmax)
-    dists.extend(list(filter(lambda x: x > 0, map(lambda n: get_dist(structure.sites[i].coords, n.coords), neighbors))))
-  return sum(map(lambda d: norm.pdf(rs, d, σ), dists)) / (p * (4/3) * np.pi * rs ** 3)
+    dists.extend(list(filter(lambda x: x > 0, 
+      map(lambda n: get_dist(structure.sites[i].coords, n.coords), neighbors))))
+  return sum(map(lambda d: norm_pdf[
+    (14000 - round(1000 * dists)):(24001 - round(1000 * dists))], dists)
+    ) / (p * (4/3) * np.pi * rs ** 3)
