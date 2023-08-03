8b85853691a31355d14a67f7b1e246bef0bf4d34
itsgt <islagle3@gatech.edu>
2023-08-03 15:02:05 -0400

    Optimized for speed

--- activestructopt/dataset/rdf.py
+++ activestructopt/dataset/rdf.py
@@ -1,23 +1,20 @@
 from scipy.stats import norm
 import numpy as np
 
-def get_dist(a, b):
-  return np.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2)
-
 def get_rdf(structure, σ = 0.05, dr = 0.01, max_r = 12.0):
-  rs = np.arange(0.5, max_r + 5 * σ + dr, dr)
-  p = len(structure) / structure.volume
-  rmax = np.max(rs) + 3 * σ
-  dists = []
+  rmax = max_r + 3 * σ + dr
+  rs = np.arange(0.5, rmax, dr)
+  normalization = 4 / structure.volume * np.pi 
+  normalization *= (len(structure) * rs) ** 2
+  rdf = np.zeros(len(rs))
+  site_fcoords = np.mod(structure.frac_coords, 1)
   for i in range(len(structure)):
-    neighbors = structure.get_sites_in_sphere(structure.sites[i].coords, rmax)
-    dists.extend(list(filter(lambda x: x > 0, 
-      map(lambda n: get_dist(structure.sites[i].coords, n.coords), neighbors))))
-  rdf = []
-  dists = np.array(dists)
-  for r in rs:
-    inds = dists < (r + dr)
-    rdf.append(np.sum(inds) / (len(structure) * p * 4 * np.pi * r ** 2))
-    dists = dists[np.invert(inds)]
-  return np.convolve(rdf, norm.pdf(np.arange(-5 * σ, 5 * σ + dr, dr), 0.0, σ), 
-    mode="same")[0:(len(rs) - int(np.floor((5 * σ) / dr)) - 1)]
+    # modified from pymatgen's get_site_in_spheres
+    for p in structure._lattice.get_points_in_sphere(site_fcoords, 
+        structure.sites[i].coords, rmax):
+      if 0.5 <= p[1]:
+        rdf[int((p[1] - 0.5) / dr)] += 1
+  
+  return np.convolve(np.array(rdf) / normalization, 
+                     norm.pdf(np.arange(-3 * σ, 3 * σ + dr, dr), 0.0, σ), 
+                     mode="same")[0:(len(rs) - int((3 * σ) / dr) - 1)]
