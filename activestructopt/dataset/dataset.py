0f98609126cccba5d036987cc61af3bbd4e01855
Ian Slagle <islagle3@gatech.edu>
2023-09-05 10:06:53 -0400

    Use k folds

--- activestructopt/dataset/dataset.py
+++ activestructopt/dataset/dataset.py
@@ -3,56 +3,35 @@ import numpy as np
 import json
 
 def write_data_splits(initial_structure, folder, optfunc, args, 
-                      perturbr = 0.05, N = 100, splits = (0.8, 0.05, 0.15)):
+                      perturbr = 0.05, N = 100, split = 0.85, k = 5):
   structures = [initial_structure.copy() for i in range(N)]
   for i in range(N):
     structures[i].perturb(perturbr)
 
   structure_indices = np.random.permutation(np.arange(N))
-  train_indices = structure_indices[:int(np.floor(splits[0] * N))]
-  val_indices = structure_indices[int(np.floor(splits[0] * N)):int(np.floor((splits[0] + splits[1]) * N))]
-  test_indices = structure_indices[int(np.floor((splits[0] + splits[1]) * N)):]
+  trainval_indices = structure_indices[:int(np.floor(split * N))]
+  kfolds = np.array_split(trainval_indices, k)
+  test_indices = structure_indices[int(np.floor(split * N)):]
 
-  data_list=[]
-  adaptor = AseAtomsAdaptor()
+  splits_to_make = [('test_data.json', test_indices)]
+  for i in range(k):
+      splits_to_make.append(('train_k' + str(i) + '.json', np.concatenate([kfolds[i] for i in range(5) if i != k])))
+      splits_to_make.append(('val_k' + str(i) + '.json', kfolds[k]))
 
-  for i in train_indices:
-      ase_crystal = adaptor.get_atoms(structures[i])
-      data_list.append({
-          'structure_id': str(i),
-          'positions': ase_crystal.get_positions().tolist(),
-          'cell': ase_crystal.get_cell().tolist(),
-          'atomic_numbers': ase_crystal.get_atomic_numbers().tolist(),
-          'y': optfunc(structures[i], **(args)).tolist(),
-      })
+  for j in range(len(splits_to_make)):
+      data_list=[]
+      adaptor = AseAtomsAdaptor()
+      for i in splits_to_make[j][1]:
+          ase_crystal = adaptor.get_atoms(structures[i])
+          data_list.append({
+              'structure_id': str(i),
+              'positions': ase_crystal.get_positions().tolist(),
+              'cell': ase_crystal.get_cell().tolist(),
+              'atomic_numbers': ase_crystal.get_atomic_numbers().tolist(),
+              'y': optfunc(structures[i], **(args)).tolist(),
+          })
 
-  with open(folder + '/train_data.json', 'w') as f:
-      json.dump(data_list, f)
-
-  for i in val_indices:
-      ase_crystal = adaptor.get_atoms(structures[i])
-      data_list.append({
-          'structure_id': str(i),
-          'positions': ase_crystal.get_positions().tolist(),
-          'cell': ase_crystal.get_cell().tolist(),
-          'atomic_numbers': ase_crystal.get_atomic_numbers().tolist(),
-          'y': optfunc(structures[i], **(args)).tolist(),
-      })
-
-  with open(folder + '/val_data.json', 'w') as f:
-      json.dump(data_list , f)
-
-  for i in test_indices:
-      ase_crystal = adaptor.get_atoms(structures[i])
-      data_list.append({
-          'structure_id': str(i),
-          'positions': ase_crystal.get_positions().tolist(),
-          'cell': ase_crystal.get_cell().tolist(),
-          'atomic_numbers': ase_crystal.get_atomic_numbers().tolist(),
-          'y': optfunc(structures[i], **(args)).tolist(),
-      })
-
-  with open(folder + '/test_data.json', 'w') as f:
-      json.dump(data_list, f)
+      with open(folder + '/' + splits_to_make[j][0], 'w') as f:
+          json.dump(data_list, f)
   
-  return structures, train_indices, val_indices, test_indices
+  return structures, kfolds, test_indices
