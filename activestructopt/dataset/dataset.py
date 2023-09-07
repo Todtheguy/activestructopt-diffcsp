27a703a6c7b9f227b7c58e30e6702cd7051db89c
itsgt <islagle3@gatech.edu>
2023-09-07 13:46:30 -0400

    formatting

--- activestructopt/dataset/dataset.py
+++ activestructopt/dataset/dataset.py
@@ -15,23 +15,24 @@ def write_data_splits(initial_structure, folder, optfunc, args,
 
   splits_to_make = [('test_data.json', test_indices)]
   for i in range(k):
-      splits_to_make.append(('train_k' + str(i) + '.json', np.concatenate([kfolds[i] for j in range(k) if j != i])))
-      splits_to_make.append(('val_k' + str(i) + '.json', kfolds[i]))
+    splits_to_make.append(('train_k' + str(i) + '.json', 
+      np.concatenate([kfolds[i] for j in range(k) if j != i])))
+    splits_to_make.append(('val_k' + str(i) + '.json', kfolds[i]))
 
   for j in range(len(splits_to_make)):
-      data_list=[]
-      adaptor = AseAtomsAdaptor()
-      for i in splits_to_make[j][1]:
-          ase_crystal = adaptor.get_atoms(structures[i])
-          data_list.append({
-              'structure_id': str(i),
-              'positions': ase_crystal.get_positions().tolist(),
-              'cell': ase_crystal.get_cell().tolist(),
-              'atomic_numbers': ase_crystal.get_atomic_numbers().tolist(),
-              'y': optfunc(structures[i], **(args)).tolist(),
-          })
+    data_list=[]
+    adaptor = AseAtomsAdaptor()
+    for i in splits_to_make[j][1]:
+      ase_crystal = adaptor.get_atoms(structures[i])
+      data_list.append({
+        'structure_id': str(i),
+        'positions': ase_crystal.get_positions().tolist(),
+        'cell': ase_crystal.get_cell().tolist(),
+        'atomic_numbers': ase_crystal.get_atomic_numbers().tolist(),
+        'y': optfunc(structures[i], **(args)).tolist(),
+      })
 
-      with open(folder + '/' + splits_to_make[j][0], 'w') as f:
-          json.dump(data_list, f)
+    with open(folder + '/' + splits_to_make[j][0], 'w') as f:
+      json.dump(data_list, f)
   
   return structures, kfolds, test_indices
