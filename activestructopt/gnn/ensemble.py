1facdc16b280cdd78049ca587fbc1c4df3a898b0
itsgt <islagle3@gatech.edu>
2023-09-07 12:03:44 -0400

    Add scalar calibration and formatting fixes

--- activestructopt/gnn/ensemble.py
+++ activestructopt/gnn/ensemble.py
@@ -3,30 +3,32 @@ from matdeeplearn.common.trainer_context import new_trainer_context
 from matdeeplearn.preprocessor.processor import process_data
 import activestructopt.gnn.dataloader
 import numpy as np
+from scipy.stats import norm
+from scipy.optimize import minimize
 
 class Runner:
-    def __init__(self):
-        self.config = None
+  def __init__(self):
+    self.config = None
 
-    def __call__(self, config, args):
-        with new_trainer_context(args=args, config=config) as ctx:
-            self.config = ctx.config
-            self.task = ctx.task
-            self.trainer = ctx.trainer
-            self.task.setup(self.trainer)
-            self.task.run()
+  def __call__(self, config, args):
+    with new_trainer_context(args=args, config=config) as ctx:
+        self.config = ctx.config
+        self.task = ctx.task
+        self.trainer = ctx.trainer
+        self.task.setup(self.trainer)
+        self.task.run()
 
-    def checkpoint(self, *args, **kwargs):
-        self.trainer.save(checkpoint_file="checkpoint.pt", training_state=True)
-        self.config["checkpoint"] = self.task.chkpt_path
-        self.config["timestamp_id"] = self.trainer.timestamp_id
+  def checkpoint(self, *args, **kwargs):
+    self.trainer.save(checkpoint_file="checkpoint.pt", training_state=True)
+    self.config["checkpoint"] = self.task.chkpt_path
+    self.config["timestamp_id"] = self.trainer.timestamp_id
 
 class ConfigSetup:
-    def __init__(self, run_mode, config_path):
-        self.run_mode = run_mode
-        self.config_path = config_path
-        self.seed = None
-        self.submit = None
+  def __init__(self, run_mode, config_path):
+      self.run_mode = run_mode
+      self.config_path = config_path
+      self.seed = None
+      self.submit = None
 
 class Ensemble:
   def __init__(self, k, config, datafolder):
@@ -34,23 +36,44 @@ class Ensemble:
     self.config = config
     self.datafolder = datafolder
     self.ensemble = [Runner() for _ in range(k)]
+    self.scalar = 1.0
   
   def train(self):
     for i in range(self.k):
-      self.config["dataset"]["src"]["train"] = self.datafolder + "/train_k" + str(i) + ".json"
-      self.config["dataset"]["src"]["val"] = self.datafolder + "/val_k" + str(i) + ".json"
-      self.config["dataset"]["src"]["test"] = self.datafolder + "/test_data.json"
+      self.config["dataset"]["src"]["train"] = self.datafolder + (
+        "/train_k" + str(i) + ".json")
+      self.config["dataset"]["src"]["val"] = self.datafolder + (
+        "/val_k" + str(i) + ".json")
+      self.config["dataset"]["src"]["test"] = self.datafolder + (
+        "/test_data.json")
       process_data(self.config["dataset"])
       self.ensemble[i](self.config, ConfigSetup('train', ''))
       self.ensemble[i].trainer.model.eval()
 
   def predict(self, structure):
-      ensemble_results = []
-      for i in range(self.k):
-        for batch in activestructopt.gnn.dataloader.DataWrapper(1, 0).get_dataloader(
-          structure, device = 'cuda'):
-          ensemble_results.append(
-              self.ensemble[i].trainer.model.forward(
-                  batch)['output'].cpu().detach().numpy()[0])
+    ensemble_results = []
+    for i in range(self.k):
+      for batch in activestructopt.gnn.dataloader.DataWrapper(
+        1, 0).get_dataloader(
+        structure, device = 'cuda'):
+        ensemble_results.append(
+            self.ensemble[i].trainer.model.forward(
+                batch)['output'].cpu().detach().numpy()[0])
 
-      return np.mean(np.array(ensemble_results), 0), np.std(np.array(ensemble_results), 0)
+    return np.mean(np.array(ensemble_results), 0), np.std(
+      np.array(ensemble_results), 0) * self.scalar
+
+  def set_scalar_calibration(self, test_structures, test_targets):
+    test_res = [self.predict(s) for s in test_structures]
+    zscores = []
+    for i in range(test_targets):
+      for j in range(len(test_targets[0])):
+        zscores.append((
+          test_res[i][0][j] - test_targets[i][j]) / test_res[i][1][j])
+    zscores = np.sort(zscores)
+    normdist = norm()
+    f = lambda x: np.trapz(np.abs(np.cumsum(np.ones(len(zscores))) / len(
+      zscores) - normdist.cdf(zscores / x[0])), normdist.cdf(zscores / x[0]))
+    self.scalar = minimize(f, [1.0]).x[0]
+    return normdist.cdf(np.sort(zscores) / self.scalar), np.cumsum(
+      np.ones(len(zscores))) / len(zscores)
