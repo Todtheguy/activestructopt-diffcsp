b49d584bee1f34cc3b57583291a1856bb0001559
Ian Slagle <islagle3@gatech.edu>
2023-09-21 14:52:59 -0400

    Move to device when making datasets

--- activestructopt/dataset/dataset.py
+++ activestructopt/dataset/dataset.py
@@ -3,12 +3,12 @@ import numpy as np
 
 def make_data_splits(initial_structure, optfunc, args, config, 
                       perturbrmin = 0.1, perturbrmax = 1.0, 
-                      N = 100, split = 0.85, k = 5):
+                      N = 100, split = 0.85, k = 5, device = 'cuda'):
   structures = [initial_structure.copy() for _ in range(N)]
   for i in range(1, N):
     structures[i].perturb(np.random.uniform(perturbrmin, perturbrmax))
   ys = [optfunc(structures[i], **(args)) for i in range(N)]
-  data = [prepare_data(structures[i], config, y = ys[i]) for i in range(N)]
+  data = [prepare_data(structures[i], config, y = ys[i]).to(device) for i in range(N)]
 
   structure_indices = np.random.permutation(np.arange(1, N))
   trainval_indices = structure_indices[:int(np.floor(split * N) - 1)]
