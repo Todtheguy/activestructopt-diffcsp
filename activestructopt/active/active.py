4fb08b56158804a066427c56e2502faeebeb8b5e
itsgt <islagle3@gatech.edu>
2023-10-25 12:25:01 -0400

    Replace RMC in active learning with basin hopping

--- activestructopt/active/active.py
+++ activestructopt/active/active.py
@@ -1,4 +1,4 @@
-from activestructopt.optimization.rmc.rmc import rmc_ucb, rmc_exploit
+from activestructopt.optimization.basinhopping.basinhopping import basinhop, ucb_loss, mse_loss
 from activestructopt.gnn.ensemble import Ensemble
 from activestructopt.dataset.dataset import make_data_splits, update_datasets
 import numpy as np
@@ -10,16 +10,14 @@ def active_learning(
     config, 
     initial_structure, 
     max_forward_calls = 100,
-    rmc_iterations = 10000,
     N = 30, 
     k = 5, 
     perturbrmin = 0.0, 
     perturbrmax = 1.0, 
     split = 1/3, 
     device = 'cuda',
-    rmcσ = 0.0025,
-    σr = 0.1,
-    λ = 1.0,
+    bh_starts = 100,
+    bh_iters_per_start = 100,
     print_mses = True,
     ):
   structures, ys, datasets, kfolds, test_indices, test_data, test_targets = make_data_splits(
@@ -43,16 +41,10 @@ def active_learning(
     ensemble = Ensemble(k, config, datasets)
     ensemble.train()
     ensemble.set_scalar_calibration(test_data, test_targets)
-    new_structure = (rmc_exploit if i == (active_steps - 1) else rmc_ucb)(
-      ensemble.predict,
-      {},
-      target,
-      rmcσ,
-      starting_structure,
-      rmc_iterations,
-      σr = σr,
-      λ = λ,
-    )
+    new_structure = basinhop(ensemble, starting_structure, target, 
+      starts = bh_starts, iters_per_start = bh_iters_per_start, 
+      method = "L-BFGS-B", 
+      loss_fn = mse_loss if i == (active_steps - 1) else ucb_loss)
     structures.append(new_structure)
     datasets, y = update_datasets(
       datasets,
---
+++ activestructopt/optimization/basinhopping/basinhopping.py
@@ -0,0 +1,90 @@
+import torch
+import math
+from scipy.optimize import basinhopping
+from activestructopt.gnn.dataloader import prepare_data
+
+def get_device(ensemble):
+  device = next(iter(ensemble.ensemble[0].trainer.model.state_dict().values(
+    ))).get_device()
+  device = 'cpu' if device == -1 else 'cuda:' + str(device)
+  return device
+
+def generate_data(structure, device, ensemble):
+  data = prepare_data(
+    structure, ensemble.config['dataset']).to(device)
+  data.pos.requires_grad_()
+  return data
+
+# https://en.wikipedia.org/wiki/Q-function
+def Q(x):
+  return torch.erfc(x / math.sqrt(2)) / 2
+
+# https://en.wikipedia.org/wiki/Noncentral_chi-squared_distribution#Cumulative_distribution_function
+def ncx2_cdf(x, λ):
+  return 1 - Q(torch.sqrt(x) - torch.sqrt(λ)) - Q(torch.sqrt(x) + torch.sqrt(λ))
+
+# Get PDF as local derivative of CDF
+def ncx2_pdf(x, λ, dx = 0.0001):
+  return (ncx2_cdf(x + dx, λ) - ncx2_cdf(x - dx, λ)) / (2 * dx)
+
+# Based on https://github.com/JuliaStats/Distributions.jl/blob/master/src/quantilealgs.jl#L50
+# if starting at mode, Newton is convergent for any unimodal continuous distribution, see:
+#   Göknur Giner, Gordon K. Smyth (2014)
+#   A Monotonically Convergent Newton Iteration for the Quantiles of any Unimodal
+#   Distribution, with Application to the Inverse Gaussian Distribution
+#   http://www.statsci.org/smyth/pubs/qinvgaussPreprint.pdf
+def quantile_newton(λ, std, p, eps = 1e-5):
+  x = λ + eps
+  for i in range(10):
+    x = x + (p - ncx2_cdf(x, λ + eps)) / ncx2_pdf(x, λ + eps, dx = 1e-6)
+  λ.retain_grad()
+  return torch.mean(x / (std ** 2))
+
+def ucb_loss(ensemble, data, target, p = 0.26, device = 'cpu'):
+  prediction = ensemble.ensemble[0].trainer.model._forward(data)
+  target = torch.tensor(target, device = device)
+  for i in range(1, ensemble.k):
+    prediction = torch.cat((prediction,
+               ensemble.ensemble[i].trainer.model._forward(data)), dim = 0)
+  mean = torch.mean(prediction, dim = 0)
+  std = ensemble.scalar * torch.std(prediction, dim = 0)
+  λs = (mean - target) ** 2 / (std ** 2)
+  ucb = quantile_newton(λs, std, p)
+  data.pos.retain_grad()
+  ucb.backward(retain_graph=True)
+  df = data.pos.grad.detach().cpu().numpy().flatten()
+  f = ucb.detach().cpu().item()
+  return f, df
+
+def mse_loss(ensemble, data, target, p = 0.26, device = 'cpu'):
+  prediction = ensemble.ensemble[0].trainer.model._forward(data)
+  target = torch.tensor(target, device = device)
+  for i in range(1, ensemble.k):
+    prediction = torch.cat((prediction,
+               ensemble.ensemble[i].trainer.model._forward(data)), dim = 0)
+  mean = torch.mean(prediction, dim = 0)
+  mse = torch.mean((mean - target) ** 2)
+  data.pos.retain_grad()
+  mse.backward(retain_graph=True)
+  df = data.pos.grad.detach().cpu().numpy().flatten()
+  f = mse.detach().cpu().item()
+  return f, df
+
+def basinhop(ensemble, starting_structure, target, 
+    starts = 100, iters_per_start = 100, method = "L-BFGS-B", 
+    loss_fn = ucb_loss):
+  x0 = starting_structure.cart_coords.flatten()
+  device = get_device(ensemble)
+  def func(x):
+    new_structure = starting_structure.copy()
+    for i in range(len(new_structure)):
+        new_structure[i].coords = x[(3 * i):(3 * (i + 1))]
+    f, df = loss_fn(ensemble, 
+        generate_data(new_structure, device, ensemble), target, device = device)
+    return f, df.tolist()
+  ret = basinhopping(func, x0, minimizer_kwargs = {"method": method, 
+    "jac": True, "options": {"maxiter": iters_per_start}}, niter = starts)
+  new_structure = starting_structure.copy()
+  for i in range(len(new_structure)):
+    new_structure[i].coords = ret.x[(3 * i):(3 * (i + 1))]
+  return new_structure
