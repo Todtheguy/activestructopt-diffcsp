f2789aa38c7f4c096b171d0d7b8144d1bf92bd9c
itsgt <islagle3@gatech.edu>
2023-11-16 15:27:36 -0500

    Rename loop iterators for consistency

--- activestructopt/optimization/basinhopping/basinhopping.py
+++ activestructopt/optimization/basinhopping/basinhopping.py
@@ -11,13 +11,13 @@ def run_adam(ensemble, target, x0, starting_structure, config, ljrmins,
   data = prepare_data(starting_structure, config, pos_grad = True).to(device)
   data.pos = x0
   optimizer = torch.optim.Adam([data.pos], lr=lr)
-  for j in range(niters):
+  for i in range(niters):
     optimizer.zero_grad(set_to_none=True)
     data.pos.requires_grad_()
     prediction = ensemble.ensemble[0].trainer.model._forward(data)
-    for i in range(1, ensemble.k):
+    for j in range(1, ensemble.k):
       prediction = torch.cat((prediction,
-                ensemble.ensemble[i].trainer.model._forward(data)), dim = 0)
+                ensemble.ensemble[j].trainer.model._forward(data)), dim = 0)
     mean = torch.mean(prediction, dim = 0)
     # last term to remove Bessel correction and match numpy behavior
     # https://github.com/pytorch/pytorch/issues/1082
@@ -27,11 +27,11 @@ def run_adam(ensemble, target, x0, starting_structure, config, ljrmins,
     s = torch.sqrt(2 * torch.sum((std ** 4) + 2 * (std ** 2) * (
       (target - mean) ** 2))) / (len(target))
     ucb = yhat - λ * s + lj_repulsion(data, ljrmins)
-    if j != niters - 1:
+    if i != niters - 1:
       ucb.backward()
       optimizer.step()
-    xs[j] = data.pos.detach().flatten()
-    ucbs[j] = ucb.detach().item()
+    xs[i] = data.pos.detach().flatten()
+    ucbs[i] = ucb.detach().item()
     yhat, s, mean, std, prediction, ucb = yhat.detach(), s.detach(), mean.detach(), std.detach(), prediction.detach(), ucb.detach()
     del yhat, s, mean, std, prediction, ucb
     
@@ -50,20 +50,20 @@ def basinhop(ensemble, starting_structure, target, config,
   x0 = torch.tensor(starting_structure.lattice.get_cartesian_coords(
     starting_structure.frac_coords), device = device, dtype = torch.float)
 
-  for j in range(nhops):
+  for i in range(nhops):
     new_ucbs, new_xs = run_adam(ensemble, target, x0, starting_structure, 
       config, ljrmins, niters = niters, λ = λ, lr = lr, device = device)
     
-    ucbs[j] = new_ucbs
-    xs[j] = new_xs
-    if not j + 1 == nhops:
-      accepted = xs[j][-1] if j == 0 or np.log(np.random.rand()) < (
-        ucbs[j - 1][-1] - ucbs[j][-1]) / (2 * rmcσ ** 2) else accepted
+    ucbs[i] = new_ucbs
+    xs[i] = new_xs
+    if not i + 1 == nhops:
+      accepted = xs[i][-1] if i == 0 or np.log(np.random.rand()) < (
+        ucbs[i - 1][-1] - ucbs[i][-1]) / (2 * rmcσ ** 2) else accepted
       rejected = True
       while rejected:
         hop = starting_structure.copy()
-        for i in range(len(hop)):
-          hop[i].coords = accepted[(3 * i):(3 * (i + 1))]
+        for j in range(len(hop)):
+          hop[j].coords = accepted[(3 * j):(3 * (j + 1))]
         hop.perturb(step_size)
         rejected = lj_reject(hop)
       x0 = torch.tensor(hop.lattice.get_cartesian_coords(hop.frac_coords), 
