ff7d9248874c2802995425b978ad21b107b19ca5
itsgt <islagle3@gatech.edu>
2023-11-21 06:04:28 -0500

    Fix rejection again

--- activestructopt/optimization/shared/constraints.py
+++ activestructopt/optimization/shared/constraints.py
@@ -29,6 +29,6 @@ def lj_reject(structure):
   for i in range(len(structure)):
     for j in range(i + 1, len(structure)):
       if structure.sites[i].distance(structure.sites[j]) < lj_rmins[get_z(
-        structure.sites[i])][get_z(structure.sites[j])]:
+        structure.sites[i]) - 1][get_z(structure.sites[j]) - 1]:
         return True
   return False
