24cbc7fe010a5ff9be80ec8267647cafce0550bb
itsgt <islagle3@gatech.edu>
2023-12-14 16:48:29 -0500

    Flatten constraints above rmin and increase

--- activestructopt/optimization/shared/constraints.py
+++ activestructopt/optimization/shared/constraints.py
@@ -10,18 +10,23 @@ el_symbols = np.array([periodictable.elements[i].symbol for i in range(95)])
 def get_z(site):
   return np.argmax(el_symbols == site.species.elements[0].symbol)
 
-def lj_repulsion(data, ljrmins, scale = 4000):
+def lj_repulsion(data, ljrmins, scale = 400):
   rmins = ljrmins[(data.z[data.edge_index[0]] - 1), 
     (data.z[data.edge_index[1]] - 1)]
-  return torch.mean(torch.pow(rmins / data.edge_weight, 12)) / scale
+  repulsions = torch.where(rmins <= data.edge_weight, 
+    torch.pow(rmins / data.edge_weight, 12), 1.0)
+  return (torch.mean(repulsions) - 1) / scale
 
-def lj_repulsion_pymatgen(structure, scale = 4000):
+def lj_repulsion_pymatgen(structure, scale = 400):
   repulsions = []
   for i in range(len(structure)):
     for j in range(i, len(structure)):
-      rmin = lj_rmins[get_z(structure.sites[i]) - 1, get_z(structure.sites[j]) - 1]
-      r = np.min([structure.lattice.a, structure.lattice.b, structure.lattice.c]) if i == j else structure.sites[i].distance(structure.sites[j])
-      repulsions.append((rmin / r) ** 12)
+      rmin = lj_rmins[get_z(structure.sites[i]) - 1, get_z(
+        structure.sites[j]) - 1]
+      r = np.min([structure.lattice.a, structure.lattice.b, 
+        structure.lattice.c]) if i == j else structure.sites[i].distance(
+        structure.sites[j])
+      repulsions.append(max(0, (rmin / r) ** 12 - 1))
   return np.mean(repulsions) / scale
 
 
