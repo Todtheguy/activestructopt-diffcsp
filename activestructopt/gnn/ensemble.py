50b85851757441522316acce2679512ac21e8ff7
Ian Slagle <islagle3@gatech.edu>
2024-01-16 15:44:59 -0500

    Use torch_geometric's DataLoader

--- activestructopt/gnn/ensemble.py
+++ activestructopt/gnn/ensemble.py
@@ -7,6 +7,7 @@ from torch_geometric import compile
 import torch
 from torch.func import stack_module_state, functional_call, vmap
 import copy
+from torch_geometric.loader import DataLoader
 
 class Runner:
   def __init__(self):
@@ -66,7 +67,7 @@ class Ensemble:
     data = structure if prepared else [prepare_data(
       structure, self.config['dataset']).to(self.device)]
     prediction = vmap(fmodel, in_dims = (0, 0, None))(
-      self.params, self.buffers, next(iter(torch.utils.data.DataLoader(data, batch_size = len(data)))))
+      self.params, self.buffers, next(iter(DataLoader(data, batch_size = len(data)))))
     print(prediction.size())
 
     mean = torch.mean(prediction, dim = 0)
