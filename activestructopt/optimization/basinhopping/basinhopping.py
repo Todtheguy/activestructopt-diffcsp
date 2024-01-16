6a74b7a34432089d9aff68820d03596c01895422
Ian Slagle <islagle3@gatech.edu>
2024-01-16 14:22:17 -0500

    Batch starts together in opt

--- activestructopt/optimization/basinhopping/basinhopping.py
+++ activestructopt/optimization/basinhopping/basinhopping.py
@@ -3,36 +3,42 @@ import numpy as np
 from activestructopt.gnn.dataloader import prepare_data
 from activestructopt.optimization.shared.constraints import lj_rmins, lj_repulsion, lj_reject
 
-def run_adam(ensemble, target, starting_structure, config, ljrmins,
+def run_adam(ensemble, target, starting_structures, config, ljrmins,
                     niters = 100, λ = 1.0, lr = 0.01, device = 'cpu'):
-  x0 = torch.tensor(starting_structure.lattice.get_cartesian_coords(
-        starting_structure.frac_coords), device = device, dtype = torch.float)
-  ucbs = torch.zeros(niters, device = device)
-  xs = torch.zeros((niters, 3 * x0.size()[0]), device = device)
+  nstarts = len(starting_structures)
+  natoms = len(starting_structures[0])
+  best_ucb = torch.tensor([float('inf')]), device = device)
+  best_x = torch.zeros(3 * natoms, device = device)
   target = torch.tensor(target, device = device)
-  data = prepare_data(starting_structure, config, pos_grad = True).to(device)
-  data.pos = x0
-  optimizer = torch.optim.Adam([data.pos], lr=lr)
+  data = [prepare_data(s, config, pos_grad = True).to(device) for s in starting_structures]
+  for i in range(len(starting_structures)):
+    data[i].pos = torch.tensor(starting_structures[i].lattice.get_cartesian_coords(
+        starting_structures[i].frac_coords), device = device, dtype = torch.float)
+  optimizer = torch.optim.Adam([d.pos for d in data], lr=lr)
   for i in range(niters):
     optimizer.zero_grad(set_to_none=True)
-    data.pos.requires_grad_()
-    mean, std = ensemble.predict(data, prepared = True)
-    yhat = torch.mean((std ** 2) + ((target - mean) ** 2))
-    s = torch.sqrt(2 * torch.sum((std ** 4) + 2 * (std ** 2) * (
-      (target - mean) ** 2))) / (len(target))
-    ucb = yhat - λ * s + lj_repulsion(data, ljrmins)
+    for j in range(len(starting_structures)):
+      data[j].pos.requires_grad_()
+    predictions = ensemble.predict(data, prepared = True)
+    ucbs = torch.zeros(len(starting_structures))
+    for j in range(len(starting_structures)):
+      yhat = torch.mean((predictions[j, 1] ** 2) + ((target - predictions[j, 0]) ** 2))
+      s = torch.sqrt(2 * torch.sum((predictions[j, 1] ** 4) + 2 * (predictions[j, 1] ** 2) * (
+        (target - predictions[j, 0]) ** 2))) / (len(target))
+      ucbs[j] = yhat - λ * s + lj_repulsion(data, ljrmins)
     if i != niters - 1:
-      ucb.backward()
+      for j in range(len(starting_structures)):
+        ucbs[j].backward()
       optimizer.step()
-    xs[i] = data.pos.detach().flatten()
-    ucbs[i] = ucb.detach().item()
-    yhat, s, mean, std, ucb = yhat.detach(), s.detach(
-      ), mean.detach(), std.detach(), ucb.detach()
-    del yhat, s, mean, std, ucb
+    if (torch.min(ucbs) < best_ucb).item():
+      best_ucb = torch.min(ucbs).detach()
+      best_x = data[torch.argmin(ucbs).item()].pos.detach().flatten()
+    yhat, s, mean, std, ucbs = yhat.detach(), s.detach(
+      ), mean.detach(), std.detach(), ucbs.detach()
+    del yhat, s, mean, std, ucbs
     
-  to_return = ucbs.detach().cpu().numpy(), xs.detach().cpu().numpy()
-  del ucbs, xs, target, data
-  to_return = to_return[0][np.argmin(to_return[0])], to_return[1][np.argmin(to_return[0])]
+  to_return = best_x.detach().cpu().numpy() 
+  del best_ucb, best_x, target, data
   return to_return
 
 def basinhop(ensemble, starting_structures, target, config,
@@ -43,13 +49,10 @@ def basinhop(ensemble, starting_structures, target, config,
   xs = np.zeros((nhops, 3 * len(starting_structures[0])))
   ljrmins = torch.tensor(lj_rmins, device = device)
 
-  for i in range(nhops):
-    new_ucb, new_x = run_adam(ensemble, target, starting_structures[i], 
-      config, ljrmins, niters = niters, λ = λ, lr = lr, device = device)
-    
-    ucbs[i] = new_ucb
-    xs[i] = new_x
+  new_x = run_adam(ensemble, target, starting_structures, 
+    config, ljrmins, niters = niters, λ = λ, lr = lr, device = device)
+  
   new_structure = starting_structures[0].copy()
   for i in range(len(new_structure)):
-    new_structure[i].coords = xs[np.argmin(ucbs)][(3 * i):(3 * (i + 1))]
+    new_structure[i].coords = new_x[(3 * i):(3 * (i + 1))]
   return new_structure
