1405a218370da78425e3d1d42ae29700f644b2aa
itsgt <islagle3@gatech.edu>
2024-05-29 13:00:28 -0400

    Save predictions of the target structure over time

--- activestructopt/active/active.py
+++ activestructopt/active/active.py
@@ -10,12 +10,15 @@ from os.path import join as pathjoin
 
 class ActiveLearning():
   def __init__(self, simfunc, target, config, initial_structure, 
-    optfunc = basinhop, index = -1):
+    optfunc = basinhop, index = -1, target_structure = None):
     self.simfunc = simfunc
     self.config = simfunc.setup_config(config)
     self.optfunc = optfunc
     self.index = index
     self.gnn_maes = []
+    self.target_structure = target_structure
+    if not (target_structure is None):
+      self.target_predictions = []
 
     self.dataset = ASODataset(initial_structure, target, simfunc, 
       config['dataset'], **(config['aso_params']['dataset']))
@@ -39,6 +42,9 @@ class ActiveLearning():
       
       gnn_mae, _, _ = self.ensemble.set_scalar_calibration(self.dataset)
       self.gnn_maes.append(gnn_mae)
+      if not (self.target_structure is None):
+        self.target_predictions.append(self.ensemble.predict(
+          self.target_structure).cpu().numpy())
       
       opt_profile = self.config['aso_params']['opt']['profiles'][
         np.searchsorted(-np.array(
@@ -70,6 +76,8 @@ class ActiveLearning():
           'ys': self.dataset.ys,
           'mismatches': self.dataset.mismatches,
           'gnn_maes': self.gnn_maes,}
+    if not (self.target_structure is None):
+      res['target_predictions'] = self.target_predictions
     for k, v in additional_data.items():
       res[k] = v
     with open(filename, "wb") as file:
