d3618c7c9a3a32063f0c14feebc55dd75c8ab203
itsgt <islagle3@gatech.edu>
2024-05-31 18:12:02 -0400

    Clean up registry

--- activestructopt/common/registry.py
+++ activestructopt/common/registry.py
@@ -18,69 +18,21 @@ Various decorators for registry different kind of classes with unique keys
 import importlib
 from pathlib import Path
 
-def _get_absolute_mapping(name: str):
-    # in this case, the `name` should be the fully qualified name of the class
-    # e.g., `matdeeplearn.tasks.base_task.BaseTask`
-    # we can use importlib to get the module 
-    # and then import the class (e.g., `BaseTask`)
-
-    module_name = ".".join(name.split(".")[:-1])
-    class_name = name.split(".")[-1]
-
-    try:
-        module = importlib.import_module(module_name)
-    except (ModuleNotFoundError, ValueError) as e:
-        raise RuntimeError(
-            f"Could not import module {module_name=} for import {name=}"
-        ) from e
-
-    try:
-        return getattr(module, class_name)
-    except AttributeError as e:
-        raise RuntimeError(
-            f"Could not import class {class_name=} from module {module_name=}"
-        ) from e
-
-def _import_local_file(path: Path, *, project_root: Path):
-    """
-    Imports a Python file as a module
-
-    :param path: The path to the file to import
-    :type path: Path
-    :param project_root: The root directory of the project (i.e., the "activestructopt" folder)
-    :type project_root: Path
-    """
-
-    path = path.resolve()
-    project_root = project_root.resolve()
-
-    module_name = ".".join(
-        path.absolute().relative_to(project_root.absolute()).with_suffix("").parts
-    )
-    # logging.debug(f"Resolved module name of {path} to {module_name}")
-    importlib.import_module(module_name)
-
-
-def _get_project_root():
-    """
-    Gets the root folder of the project (the parent of the "activestructopt" folder)
-    :return: The absolute path to the project root.
-    """
+# Copied from 
+#     https://github.com/facebookresearch/mmf/blob/master/mmf/utils/env.py#L89.
+def setup_imports():
     root_folder = Path(__file__).parent
     while root_folder.stem != 'activestructopt':
         root_folder = root_folder.parent
-    return root_folder.parent
-
-# Copied from https://github.com/facebookresearch/mmf/blob/master/mmf/utils/env.py#L89.
-def setup_imports():
-    project_root = _get_project_root()
+    project_root = root_folder.parent.resolve().absolute()
 
     import_keys = ["dataset", "model", "objective", "optimizer", "simulation"]
     for key in import_keys:
         dir_list = (project_root / "activestructopt" / key).rglob("*.py")
         for f in dir_list:
-            _import_local_file(f, project_root = project_root)
-
+            module_name = ".".join(f.resolve().absolute().relative_to(
+                project_root).with_suffix("").parts)
+            importlib.import_module(module_name)
 
 class Registry:
     r"""Class for registry object which acts as central source of truth."""
@@ -130,14 +82,7 @@ class Registry:
 
     @classmethod
     def get_class(cls, name: str, mapping_name: str):
-        existing_mapping = cls.mapping[mapping_name].get(name, None)
-        if existing_mapping is not None:
-            return existing_mapping
-
-        # mapping be class path of type `{module_name}.{class_name}`
-        #assert name.count(".") >= 1
-
-        return _get_absolute_mapping(name)
+        return cls.mapping[mapping_name].get(name, None)
 
     @classmethod
     def get_dataset_class(cls, name):
