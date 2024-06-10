974319349336c85212beea2968d7ff682f8982b6
itsgt <islagle3@gatech.edu>
2024-06-10 16:39:46 -0400

    Add random optimizer

---
+++ activestructopt/optimizer/random.py
@@ -0,0 +1,16 @@
+from activestructopt.model.base import BaseModel
+from activestructopt.dataset.base import BaseDataset
+from activestructopt.objective.base import BaseObjective
+from activestructopt.optimizer.base import BaseOptimizer
+from activestructopt.sampler.base import BaseSampler
+from activestructopt.common.registry import registry
+from pymatgen.core.structure import IStructure
+
+@registry.register_optimizer("Random")
+class Torch(BaseOptimizer):
+  def __init__(self) -> None:
+    pass
+
+  def run(self, model: BaseModel, dataset: BaseDataset, 
+    objective: BaseObjective, sampler: BaseSampler,  **kwargs) -> IStructure:
+    return sampler.sample(), None
