b1652cdccfe3ee6f00047e523a4f833ebe3e0e75
itsgt <islagle3@gatech.edu>
2024-06-18 23:26:49 -0400

    sampling lattice lengths are percentages

--- activestructopt/sampler/perturbation.py
+++ activestructopt/sampler/perturbation.py
@@ -21,12 +21,12 @@ class Perturbation(BaseSampler):
       new_structure.perturb(np.random.uniform(
         self.perturbrmin, self.perturbrmax))
       new_structure.lattice = new_structure.lattice.from_parameters(
-        max(0.0, new_structure.lattice.a + np.random.uniform(
-          -self.perturblmax, self.perturblmax)),
-        max(0.0, new_structure.lattice.b + np.random.uniform(
-          -self.perturblmax, self.perturblmax)),
-        max(0.0, new_structure.lattice.c + np.random.uniform(
-          -self.perturblmax, self.perturblmax)), 
+        max(0.0, new_structure.lattice.a * np.random.uniform(
+          1 - self.perturblmax, 1 + self.perturblmax)),
+        max(0.0, new_structure.lattice.b * np.random.uniform(
+          1 - self.perturblmax, 1 + self.perturblmax)),
+        max(0.0, new_structure.lattice.c * np.random.uniform(
+          1 - self.perturblmax, 1 + self.perturblmax)), 
         min(180.0, max(0.0, new_structure.lattice.alpha + np.random.uniform(
           -self.perturbθmax, self.perturbθmax))), 
         min(180.0, max(0.0, new_structure.lattice.beta + np.random.uniform(
