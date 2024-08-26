e4e51a3d17e0da1ca151da8122fc120f0e566c25
itsgt <islagle3@gatech.edu>
2024-08-26 12:01:59 -0400

    Also print lattice

--- activestructopt/optimizer/torch.py
+++ activestructopt/optimizer/torch.py
@@ -121,6 +121,7 @@ class Torch(BaseOptimizer):
         try:
           new_structure[i].coords = new_x[(3 * i):(3 * (i + 1))]
         except np.linalg.LinAlgError as e:
+          print(new_structure.lattice)
           print(new_x)
           raise e
 
