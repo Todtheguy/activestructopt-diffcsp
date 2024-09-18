1a46d12263fa05018587f2a1d88e97ede0d77d2b
itsgt <islagle3@gatech.edu>
2024-09-18 19:32:46 -0400

    SA perturb either perturbs one atom or the lattice

--- activestructopt/sampler/single_atom_perturbation.py
+++ activestructopt/sampler/single_atom_perturbation.py
@@ -7,12 +7,13 @@ import numpy as np
 @registry.register_sampler("SingleAtomPerturbation")
 class SingleAtomPerturbation(BaseSampler):
   def __init__(self, initial_structure: IStructure, perturbrmin = 0.1, 
-    perturbrmax = 1.0, perturblmax = 0.0, perturbθmax = 0.0) -> None:
+    perturbrmax = 1.0, perturblmax = 0.0, perturbθmax = 0.0, lattice_prob = 0.5) -> None:
     self.initial_structure = initial_structure
     self.perturbrmin = perturbrmin
     self.perturbrmax = perturbrmax
     self.perturblmax = perturblmax
     self.perturbθmax = perturbθmax
+    self.lattice_prob = lattice_prob
 
   def sample(self) -> IStructure:
     rejected = True
@@ -22,24 +23,25 @@ class SingleAtomPerturbation(BaseSampler):
       new_structure2.perturb(np.random.uniform(
         self.perturbrmin, self.perturbrmax))
 
-      rand_index = np.random.randint(0, len(new_structure))
+      if np.random.rand() > self.lattice_prob:
+        rand_index = np.random.randint(0, len(new_structure))
 
-      new_structure.sites[rand_index].frac_coords = new_structure2.sites[
-        rand_index].frac_coords
-      
-      new_structure.lattice = new_structure.lattice.from_parameters(
-        max(0.0, new_structure.lattice.a * np.random.uniform(
-          1 - self.perturblmax, 1 + self.perturblmax)),
-        max(0.0, new_structure.lattice.b * np.random.uniform(
-          1 - self.perturblmax, 1 + self.perturblmax)),
-        max(0.0, new_structure.lattice.c * np.random.uniform(
-          1 - self.perturblmax, 1 + self.perturblmax)), 
-        min(180.0, max(0.0, new_structure.lattice.alpha + np.random.uniform(
-          -self.perturbθmax, self.perturbθmax))), 
-        min(180.0, max(0.0, new_structure.lattice.beta + np.random.uniform(
-          -self.perturbθmax, self.perturbθmax))), 
-        min(180.0, max(0.0, new_structure.lattice.gamma + np.random.uniform(
-          -self.perturbθmax, self.perturbθmax)))
-      )
+        new_structure.sites[rand_index].frac_coords = new_structure2.sites[
+          rand_index].frac_coords
+      else:
+        new_structure.lattice = new_structure.lattice.from_parameters(
+          max(0.0, new_structure.lattice.a * np.random.uniform(
+            1 - self.perturblmax, 1 + self.perturblmax)),
+          max(0.0, new_structure.lattice.b * np.random.uniform(
+            1 - self.perturblmax, 1 + self.perturblmax)),
+          max(0.0, new_structure.lattice.c * np.random.uniform(
+            1 - self.perturblmax, 1 + self.perturblmax)), 
+          min(180.0, max(0.0, new_structure.lattice.alpha + np.random.uniform(
+            -self.perturbθmax, self.perturbθmax))), 
+          min(180.0, max(0.0, new_structure.lattice.beta + np.random.uniform(
+            -self.perturbθmax, self.perturbθmax))), 
+          min(180.0, max(0.0, new_structure.lattice.gamma + np.random.uniform(
+            -self.perturbθmax, self.perturbθmax)))
+        )
       rejected = lj_reject(new_structure)
     return new_structure
