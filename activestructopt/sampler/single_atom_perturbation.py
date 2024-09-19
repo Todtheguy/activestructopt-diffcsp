0501ef84519eedac49eb268307bf56981d3bf21d
itsgt <islagle3@gatech.edu>
2024-09-18 23:26:21 -0400

    One lattice parameter at a time

--- activestructopt/sampler/single_atom_perturbation.py
+++ activestructopt/sampler/single_atom_perturbation.py
@@ -29,19 +29,20 @@ class SingleAtomPerturbation(BaseSampler):
         new_structure.sites[rand_index].frac_coords = new_structure2.sites[
           rand_index].frac_coords
       else:
+        to_change = np.random.randint(6)
         new_structure.lattice = new_structure.lattice.from_parameters(
           max(0.0, new_structure.lattice.a * np.random.uniform(
-            1 - self.perturblmax, 1 + self.perturblmax)),
+            1 - self.perturblmax, 1 + self.perturblmax)) if to_change == 0 else new_structure.lattice.a,
           max(0.0, new_structure.lattice.b * np.random.uniform(
-            1 - self.perturblmax, 1 + self.perturblmax)),
+            1 - self.perturblmax, 1 + self.perturblmax)) if to_change == 1 else new_structure.lattice.b,
           max(0.0, new_structure.lattice.c * np.random.uniform(
-            1 - self.perturblmax, 1 + self.perturblmax)), 
+            1 - self.perturblmax, 1 + self.perturblmax)) if to_change == 2 else new_structure.lattice.c, 
           min(180.0, max(0.0, new_structure.lattice.alpha + np.random.uniform(
-            -self.perturbθmax, self.perturbθmax))), 
+            -self.perturbθmax, self.perturbθmax))) if to_change == 3 else new_structure.lattice.alpha, 
           min(180.0, max(0.0, new_structure.lattice.beta + np.random.uniform(
-            -self.perturbθmax, self.perturbθmax))), 
+            -self.perturbθmax, self.perturbθmax))) if to_change == 4 else new_structure.lattice.beta, 
           min(180.0, max(0.0, new_structure.lattice.gamma + np.random.uniform(
-            -self.perturbθmax, self.perturbθmax)))
+            -self.perturbθmax, self.perturbθmax))) if to_change == 5 else new_structure.lattice.gamma
         )
       rejected = lj_reject(new_structure)
     return new_structure
