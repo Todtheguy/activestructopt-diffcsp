409b8b502a3370c6897d03f429f7802972485ce5
itsgt <islagle3@gatech.edu>
2024-09-20 15:58:19 -0400

    Add high temperature test

---
+++ activestructopt/testing/test.py
@@ -0,0 +1,31 @@
+from activestructopt.simulation.rdf import RDF
+from activestructopt.active.config import torchmd_config
+from activestructopt.active.active import ActiveLearning
+import copy
+import numpy as np
+import os
+import sys
+from pymatgen.io.cif import CifParser
+
+def main():
+	config = torchmd_config
+	pristine_structure = CifParser("datasets/ht/start/" + str(sys.argv[1]) + ".cif"
+		).get_structures(primitive = False)[0]
+	target_structure = CifParser("datasets/ht/target/" + str(sys.argv[1]) + ".cif"
+		).get_structures(primitive = False)[-1]
+
+	rdf_func = RDF(pristine_structure, σ = 0.1, max_r = 12.)
+
+	target_promise = copy.deepcopy(rdf_func)
+	target_promise.get(target_structure)
+	target_spec = target_promise.resolve()
+	target_spec = np.mean(target_spec[np.array(rdf_func.mask)], axis = 0)
+
+	al = ActiveLearning(rdf_func, target_spec, config, pristine_structure, 
+		index = sys.argv[1], target_structure = target_structure)
+	al.optimize(save_progress_dir = 'progress')
+	al.save("res/" + str(sys.argv[1]) + ".pkl", 
+		additional_data = {'target_structure': target_structure})
+
+if __name__ == "__main__":
+    main()
