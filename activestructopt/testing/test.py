c5580a050e1bcc3a2a23360fc711834431f94fc8
Ian Slagle <islagle3@gatech.edu>
2024-09-23 14:17:01 -0400

    Update test.py

--- activestructopt/testing/test.py
+++ activestructopt/testing/test.py
@@ -9,9 +9,9 @@ from pymatgen.io.cif import CifParser
 
 def main():
 	config = torchmd_config
-	pristine_structure = CifParser("datasets/ht/start/" + str(sys.argv[1]) + ".cif"
+	pristine_structure = CifParser("~/ActiveStructOpt/activestructopt/testing/datasets/ht/start/" + str(sys.argv[1]) + ".cif"
 		).get_structures(primitive = False)[0]
-	target_structure = CifParser("datasets/ht/target/" + str(sys.argv[1]) + ".cif"
+	target_structure = CifParser("~/ActiveStructOpt/activestructopt/testing/datasets/ht/target/" + str(sys.argv[1]) + ".cif"
 		).get_structures(primitive = False)[-1]
 
 	rdf_func = RDF(pristine_structure, σ = 0.1, max_r = 12.)
@@ -24,7 +24,7 @@ def main():
 	al = ActiveLearning(rdf_func, target_spec, config, pristine_structure, 
 		index = sys.argv[1], target_structure = target_structure)
 	al.optimize(save_progress_dir = 'progress')
-	al.save("res/" + str(sys.argv[1]) + ".pkl", 
+	al.save("~/ActiveStructOpt/activestructopt/testing/res/" + str(sys.argv[1]) + ".pkl", 
 		additional_data = {'target_structure': target_structure})
 
 if __name__ == "__main__":
