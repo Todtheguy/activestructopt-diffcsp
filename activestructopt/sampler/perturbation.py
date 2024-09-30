409c3f5b2e44e74fd91db570f316e90006e12050
itsgt <islagle3@gatech.edu>
2024-09-30 11:32:39 -0400

    Perturb lattice by Guassian on lattice matrix

--- activestructopt/sampler/perturbation.py
+++ activestructopt/sampler/perturbation.py
@@ -1,18 +1,17 @@
 from activestructopt.sampler.base import BaseSampler
 from activestructopt.common.registry import registry
 from activestructopt.common.constraints import lj_reject
-from pymatgen.core.structure import IStructure
+from pymatgen.core.structure import IStructure, Lattice
 import numpy as np
 
 @registry.register_sampler("Perturbation")
 class Perturbation(BaseSampler):
   def __init__(self, initial_structure: IStructure, perturbrmin = 0.1, 
-    perturbrmax = 1.0, perturblmax = 0.0, perturbθmax = 0.0) -> None:
+    perturbrmax = 1.0, perturblσ = 0.1) -> None:
     self.initial_structure = initial_structure
     self.perturbrmin = perturbrmin
     self.perturbrmax = perturbrmax
-    self.perturblmax = perturblmax
-    self.perturbθmax = perturbθmax
+    self.perturblσ = perturblσ
 
   def sample(self) -> IStructure:
     rejected = True
@@ -21,20 +20,8 @@ class Perturbation(BaseSampler):
         new_structure = self.initial_structure.copy()
         new_structure.perturb(np.random.uniform(
           self.perturbrmin, self.perturbrmax))
-        new_structure.lattice = new_structure.lattice.from_parameters(
-          max(0.0, new_structure.lattice.a * np.random.uniform(
-            1 - self.perturblmax, 1 + self.perturblmax)),
-          max(0.0, new_structure.lattice.b * np.random.uniform(
-            1 - self.perturblmax, 1 + self.perturblmax)),
-          max(0.0, new_structure.lattice.c * np.random.uniform(
-            1 - self.perturblmax, 1 + self.perturblmax)), 
-          min(180.0, max(0.0, new_structure.lattice.alpha + np.random.uniform(
-            -self.perturbθmax, self.perturbθmax))), 
-          min(180.0, max(0.0, new_structure.lattice.beta + np.random.uniform(
-            -self.perturbθmax, self.perturbθmax))), 
-          min(180.0, max(0.0, new_structure.lattice.gamma + np.random.uniform(
-            -self.perturbθmax, self.perturbθmax)))
-        )
+        new_structure.lattice = Lattice(new_structure.lattice.matrix + 
+          self.perturblσ * np.random.normal(0, 1, (3, 3)))
         rejected = lj_reject(new_structure)
       except:
         rejected = True
