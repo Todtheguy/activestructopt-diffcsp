f7cbde2828ffeed7526fdace0b8b80524b79d746
Akshay Sanjeev Daftardar <adaftardar3@atl1-1-03-011-23-0.pace.gatech.edu>
2024-11-01 14:32:04 -0400

    created diffusion sampler for aso

--- activestructopt/sampler/diffusion.py
+++ activestructopt/sampler/diffusion.py
@@ -9,14 +9,14 @@ from types import SimpleNamespace
 @registry.register_sampler("Diffusion")
 class Diffusion(BaseSampler):
     def __init__(self, initial_structure: IStructure) -> None:
-    self.model_path = '/home/hice1/adaftardar3/DiffCSP-Forked'
-    self.save_path = '/home/hice1/adaftardar3/DiffCSP-Forked'
-    self.forumla = initial_structure.formula
-    self.num_evals = 10
-    self.batch_size = 500
-    self.step_lr = 1e-5
+        self.model_path = '/home/hice1/adaftardar3/DiffCSP-Forked'
+        self.save_path = '/home/hice1/adaftardar3/DiffCSP-Forked'
+        self.formula = initial_structure.formula
+        self.num_evals = 10
+        self.batch_size = 500
+        self.step_lr = 1e-5
 
-  def sample(self) -> IStructure:
-    data = SimpleNamespace(**{'model_path': self.model_path, 'save_path': self.save_path, 'formula': self.formula, 'num_evals': self.num_evals, 'batch_size': self.batch_size, 'step_lr': self.step_lr})
-    new_structure = sample.main(data)
-    return new_structure
+    def sample(self) -> IStructure:
+        data = SimpleNamespace(**{'model_path': self.model_path, 'save_path': self.save_path, 'formula': self.formula, 'num_evals': self.num_evals, 'batch_size': self.batch_size, 'step_lr': self.step_lr})
+        new_structure = sample.main(data)
+        return new_structure
