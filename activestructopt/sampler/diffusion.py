6568635f7b4b18ea2b46f727ab190bf434744f7d
Karishma Thakrar <kthakrar3@atl1-1-03-010-20-0.pace.gatech.edu>
2024-11-06 21:33:28 -0500

    Updated diffusion and config files

--- activestructopt/sampler/diffusion.py
+++ activestructopt/sampler/diffusion.py
@@ -9,14 +9,8 @@ from types import SimpleNamespace
 @registry.register_sampler("Diffusion")
 class Diffusion(BaseSampler):
     def __init__(self, initial_structure: IStructure) -> None:
-        self.model_path = '/home/hice1/adaftardar3/DiffCSP-Forked'
-        self.save_path = '/home/hice1/adaftardar3/DiffCSP-Forked'
         self.formula = initial_structure.formula
-        self.num_evals = 10
-        self.batch_size = 500
-        self.step_lr = 1e-5
 
     def sample(self) -> IStructure:
-        data = SimpleNamespace(**{'model_path': self.model_path, 'save_path': self.save_path, 'formula': self.formula, 'num_evals': self.num_evals, 'batch_size': self.batch_size, 'step_lr': self.step_lr})
-        new_structure = sample.main(data)
+        new_structure = sample.main()
         return new_structure
