5bd0bdf4f6de207c077aab7d38d5a9e7a5d49407
Akshay Sanjeev Daftardar <adaftardar3@login-ice-1.pace.gatech.edu>
2024-11-17 13:09:40 -0500

    update sampler + config

--- activestructopt/sampler/diffusion.py
+++ activestructopt/sampler/diffusion.py
@@ -8,9 +8,16 @@ from types import SimpleNamespace
 
 @registry.register_sampler("Diffusion")
 class Diffusion(BaseSampler):
-    def __init__(self, initial_structure: IStructure) -> None:
-        self.formula = initial_structure.formula
+    def __init__(self, initial_structure: IStructure, model_path, save_path, num_evals, batch_size, step_lr) -> None:
+        self.formula = initial_structure.formula.replace(' ', "")
+        self.model_path = model_path
+        self.save_path = save_path
+        self.num_evals = num_evals
+        self.batch_size = batch_size
+        self.step_lr = step_lr
+        self.lengths = initial_structure.lattice.abc
+        self.angles = initial_structure.lattice.angles
 
     def sample(self) -> IStructure:
-        new_structure = sample.main()
+        new_structure = sample.main(self.model_path, self.save_path, self.formula, self.num_evals, self.batch_size, self.step_lr, self.lengths, self.angles)
         return new_structure
